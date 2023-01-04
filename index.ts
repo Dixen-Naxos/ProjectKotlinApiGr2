3bf88b243e7b0241e48226cd222e7297227c93ad
Armand <armanddailly@gmail.com>
2023-01-04 12:07:21 +0100

    Add base for server

---
+++ index.ts
@@ -0,0 +1,26 @@
+import {config} from "dotenv";
+config();
+
+import express from "express";
+import mongoose, {Mongoose} from "mongoose";
+
+async function startServer(): Promise<void> {
+
+    const m: Mongoose = await mongoose.connect(process.env.MONGO_URI as string, {
+        auth: {
+            username: process.env.MONGO_USER  as string,
+            password: process.env.MONGO_PASSWORD as string
+        }
+    });
+
+    const app = express();
+
+    //const categoryController = new CategoryController();
+    //app.use('/category', categoryController.buildRoutes());
+
+    app.listen(process.env.PORT, function() {
+        console.log("Server listening on port " + process.env.PORT);
+    });
+}
+
+startServer().catch(console.error);
